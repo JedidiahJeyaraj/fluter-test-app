6dd54112df4bfff47c89ca52a60fc6a36b72246f
Jedidiah Jeyaraj <jedidiahjeyaraj@gmail.com>
2022-05-25 22:23:31 +0530

    learned testing for flutter

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_windows/models/favorites.dart';
+import 'package:test_windows/screens/favorites.dart';
+import 'package:test_windows/screens/home.dart';
+
+void main() {
+  runApp(const TestingApp());
+}
+
+class TestingApp extends StatelessWidget {
+  const TestingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child:  MaterialApp(
+        title: 'Testing App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => const HomePage(),
+          FavoritesPage.routeName: (context) => const FavoritesPage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
+}
